bd6f87ebd9cf7623f721e8ccc798326cac38c4ff
Brad Baumel <Baumelbi@gmail.com>
2016-12-07 23:47:49 -0800

    made changes and refactored my project a bit, also the outfile has been added which rewrites over itself when the program runs

--- students/baumel/project/crimeHeatMap.py
+++ students/baumel/project/crimeHeatMap.py
@@ -26,13 +26,12 @@ from geopy.geocoders import Nominatim
 
 
 def manualLocation(geolocator):
-	userInput = input("Please enter an address or location in Seattle: ")
-	location = geolocator.geocode(userInput)
-	while "seattle" not in location.address.lower():
-		userInput = input("Please enter an address or location in Seattle: ")
-		location = geolocator.geocode(userInput)
+	prompt = "Please enter an address or location in Seattle: "
+	
+	userInput = input(prompt)
+	location = geolocator.geocode(userInput)	
 
-	results(location.longitude, location.latitude, location.address)
+	return location
 
 def ipLocation(geolocator):
 	print ("Estimating location based on IP address")
@@ -44,36 +43,88 @@ def ipLocation(geolocator):
 	
 	#44.9778, -93.2650 hard coded non seattle location to test
 	#location = geolocator.reverse("44.9778, -93.2650") 
+	
 	location = geolocator.reverse(latlong)
+	return location
 
-	if "seattle" not in location.address.lower():
-		print("You are not located in Seattle")
-		manualLocation(geolocator)
-	
-	else:
-		results(location.longitude, location.latitude, location.address)
-
-def results(longitude, latitude, address):
-	print()
-	radius = float(eval(input("What radius would you like in miles? "))*1609)
-	limit = int(eval(input("How many results would you like? (1000 - 50,000) ")))
+def radiusAndLimit():
 	goodLimit = False
-	
+	goodRadius = False
+
 	while (not goodLimit):
-		if amount >= 1000 and amount <= 50000:
-			goodLimit = True
+		limit = input("How many results would you like? (1000 - 50,000) ")
+		
+		try:
+			int(limit)
+		except ValueError:
+			print ("Invalid value entered!")
 		else:
-			limit = int(eval(input("How many results would you like? (1000 - 50,000) ")))
+			limit = int(limit)
+			if limit >= 1000 and limit <= 50000:
+				goodLimit = True
+			else:
+				print ("Invalid amount entered!")
+
+	while (not goodRadius):
+		radius = input("What radius would you like in miles? (.1 - 1000.0) ")
+		
+		try:
+			float(radius)
+		except ValueError:
+			print ("Invalid value entered!")
+		else:
+			radius = float(radius)
+			if radius >= 0.1 and radius <= 1000.0:
+				goodRadius = True
+				radius = radius * 1609 #conversion to meters for the API call
+			else:
+				print ("Invalid amount entered!")
+
+	return radius, limit
+
+def validLocation(geolocator, inputOption):
+
+
+	if inputOption == "n":
+		location = ipLocation(geolocator)
+	else:
+		location = manualLocation(geolocator)
+
+	while location is None or "seattle" not in location.address.lower():
+			location = manualLocation(geolocator)
 	
-	query = ("https://data.seattle.gov/resource/pu5n-trf4.json?$limit={}&$where=within_circle(incident_location,{},{},{})".format(limit,latitude,longitude,radius))
+	latitude = geolocator.geocode(location).latitude
+	longitude = geolocator.geocode(location).longitude
+	address = geolocator.geocode(location).address
+	radius, limit = radiusAndLimit()
 
-	raw_data = pd.read_json(query)
+	return {'location': location, 
+			'radius': radius, 
+			'limit':limit, 
+			'latitude':latitude, 
+			'longitude': longitude,
+			'address': address}
 
+	
+
+
+def apiResults(locationInfo):
+	query = ("https://data.seattle.gov/resource/pu5n-trf4.json?$limit={}&$where=within_circle(incident_location,{},{},{})"
+		.format(locationInfo['limit'],
+				locationInfo['latitude'],
+				locationInfo['longitude'],
+				locationInfo['radius']))
+
+	return pd.read_json(query)
+	
+def mapMaker(raw_data, locationInfo):
 	# taking the parsed JSon and alligning it with the map
-	gmap = gmplot.GoogleMapPlotter(latitude, longitude, 13)
+	gmap = gmplot.GoogleMapPlotter(locationInfo['latitude'],locationInfo['longitude'], 13)
 	gmap.heatmap(raw_data["latitude"], raw_data["longitude"], radius = 30)
 	gmap.draw("mymap.html")
 
+
+def output(raw_data, locationInfo):
 	# opens up the html file in your default browser
 	url = "mymap.html"
 	webbrowser.open('file://' + os.path.realpath(url))
@@ -82,24 +133,37 @@ def results(longitude, latitude, address):
 	counts = raw_data['event_clearance_group'].value_counts()
 	print()
 	print('Crime events by count in your area')
-	print(address)
+	print(locationInfo['address'])
 	print()
 	print(counts)
 
+
 def main():
 	geolocator = Nominatim()
-	go = True
+	intro = True
+	again = True
 
 	print("This app will generate a crime heat map based on your location")
 
-	while go:
-		intro = input("Would you like to enter a location(y/n): ")
-		if intro.lower() == "y":
-			go = False
-			manualLocation(geolocator)
-		elif intro.lower() == "n":
-			go = False
-			ipLocation(geolocator)
+	while intro:
+		introText = input("Would you like to enter a location (y/n) ")
+		if introText.lower() == "y" or introText.lower() == "n":
+			locationInfo = validLocation(geolocator, introText)
+			intro = False
+	
+	raw_data = apiResults(locationInfo)
+	mapMaker(raw_data, locationInfo)
+	output(raw_data, locationInfo)
+	
+	while again:
+		againText = input("Would you like to run the application again (y/n) ")
+		if againText.lower() == "y" or againText.lower() == "n":
+			again = False
+		if againText.lower() == "y":
+			main()
+		else:
+			print("Thanks! See you next time")
+			SystemExit
 		
 
 if __name__ == '__main__':
